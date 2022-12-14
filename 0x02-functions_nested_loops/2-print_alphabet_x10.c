8a3c3aa45e2a4fab7f07ae903bff39a7da75bca4
root <kalkidanaki@gmail.com>
2022-12-13 20:25:07 -0800

    while

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@
 void print_alphabet_x10(void)
 
 {
-	char c = a;
+	char c = 'a';
 	int count = 1;
 
 	while (count <= 10)
 	{
-		for (c = a; c <= z; c++)
+		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
