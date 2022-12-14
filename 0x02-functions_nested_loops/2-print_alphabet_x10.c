d8ddf58501f0d1c93e0d96e0df7b7af5af1171d7
root <kalkidanaki@gmail.com>
2022-12-13 18:25:02 -0800

    Task-2 print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c = 'a';
+	int count = 1;
+
+	while (count <= 10)
+	{
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	count++;
+	}
+
+	_putchar('\n');
+
+}
