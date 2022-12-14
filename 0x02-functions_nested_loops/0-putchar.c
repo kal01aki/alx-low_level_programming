0365b8a37644108c73949202982823ca702b2ec5
root <kalkidanaki@gmail.com>
2022-12-13 17:53:12 -0800

    Task-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,29 +1,24 @@
-#include <unistd.h>
-
-
+#include "main.h"
 
 /**
+ * main - Entry point
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
+ * Return: Always 0 (Seccess)
+ */
 
-int _putchar(char c)
+int main(void)
 
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-		return (write(1, &c, 1));
+	return (0);
 
 }
-
-Footer
-
-© 2022 GitHub, Inc.
